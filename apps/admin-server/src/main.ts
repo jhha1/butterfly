f9d8e7536990814db6a490d373d6bf3314a291a1
jhha1 <skgkwlgp@gmail.com>
2025-07-07 01:48:03 +0900

    feat: remove admin

--- apps/admin-server/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AdminServerModule } from './admin-server.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AdminServerModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
--- apps/chat-server/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ChatServerModule } from './chat-server.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ChatServerModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
